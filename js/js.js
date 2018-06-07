62aaf0c21a374abcdddd2430f37b9b1ed761812a
hsimao <e087754958@gmail.com>
2018-06-07 11:23:03 +0800

    add LocalStorage, time format, add cancel Todo、start、completed、error  event..

--- js/js.js
+++ js/js.js
@@ -13,15 +13,20 @@ var app = new Vue({
             id: '123456',
             title: '超商取貨',
             createTime: '2018/06/06 19:00:00',
+            deadline: '2018/06/10 19:00:00',
             completed: false,
             important: false
         }],
-        newTodo: '',
+        newTodoTitle: '',
+        newTodoDay: '',
+        newTodoTime: '',
         show: 'all',
         cacheTodo: {},
         cacheTitle: '',
         add: false,
-        edit: false
+        edit: false,
+        error: false,
+        errorText: ''
     },
     methods: {
         getSavedTodos() {
@@ -32,27 +37,57 @@ var app = new Vue({
                 return []
             }
         },
-
         addTodo() {
-            if (!this.newTodo.trim()) return
-            console.log(this.newTodo)
+            if (!this.newTodoTitle.trim()) {
+                this.error = true
+                this.errorText = '尚未輸入文字'
+                return
+            }
+            if (!this.newTodoDay) {
+                this.error = true
+                this.errorText = '請輸入完整時間'
+                return
+            }
+            if (!this.newTodoTime) this.newTodoTime = '00:00:00'
+            newDeadline = new Date(`${this.newTodoDay} ${this.newTodoTime}`).getTime()
+
             this.todos.push({
                 id: uuidv4(),
-                title: this.newTodo,
+                title: this.newTodoTitle,
                 createTime: new Date().getTime(),
+                deadline: newDeadline,
                 completed: false,
                 important: false
             })
-            this.newTodo = ''
             this.saveTodoInLocal()
+            this.cancelAdd()
         },
         saveTodoInLocal() {
             localStorage.setItem('todos', JSON.stringify(this.todos))
+        },
+        cancelAdd() {
+            this.newTodoTitle = ''
+            this.newTodoTime = ''
+            this.newTodoDay = ''
+            this.add = false
+        },
+        format(timestamp) {
+            const time = new Date(timestamp)
+            let s = time.getSeconds()
+            let m = time.getMinutes()
+            let h = time.getHours()
+            let d = time.getDate()
+            let M = time.getMonth()+1
+            let Y = time.getFullYear()
+            return `${Y}/${M}/${d} ${h}:${m}:${s}`
+        },
+        fileChange(value) {
+            let fileName = value.files[0].name
+            value.previousElementSibling.innerText = fileName
         }
-
-
     },
     computed: {
+
 
     },
     created(){
